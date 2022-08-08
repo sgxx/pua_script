61e230cbffa429c15d836aa433447602cc33b274
sgx <718293538@qq.com>
2022-08-08 10:01:59 +0800

    sgx

---
+++ nb.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+questions=[]
+
+with open("words.txt", "r",encoding='utf-8') as f:
+    for line in f.readlines():
+        # print(line.strip())
+        if line.startswith("Q"):
+            questions.append(line.strip())
+
+length=len(questions)
+
+for i in range(length):
+    for j in range(i+1,length):
+        if questions[i] in questions[j] or questions[j] in questions[i]:
+            print(questions[i],questions[j])
